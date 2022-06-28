cacefde0a9a05a25df8426c6bf4c23953e1a5c32
werryxgames <103959882+werryxgames@users.noreply.github.com>
2022-06-28 18:45:51 +0300

    Первый commit

---
+++ main.py
@@ -0,0 +1,382 @@
+"""Мессенджер на Python (клиент)."""
+# -*- coding: utf-8 -*-
+import tkinter as tk
+
+from json import dumps
+from json import loads
+from math import ceil
+from math import floor
+from socket import AF_INET
+from socket import SOCK_DGRAM
+from socket import socket
+from threading import Thread
+from time import sleep
+from tkinter import ttk
+from typing import Optional
+
+
+class Window:
+    """Класс окна."""
+
+    def __init__(self, tk_window: tk.Tk) -> None:
+        """Инициализация окна.
+
+        Аргументы:
+            tk_window:  Окно Tkinter.
+        """
+        self.tk_window: tk.Tk = tk_window
+        self.elements: dict = {}
+
+    def place(self, id_: str, element, *args, **kwargs) -> None:
+        """Размещает объект element.
+
+        Аргументы:
+            id_:        Уникальный идентификатор элемента.
+            element:    Элемент, который необходимо разместить.
+            *args:      Аргументы element.place().
+            **kwargs:   Позиционные аргументы element.place().
+        """
+        if id_ in self.elements:
+            raise ValueError
+
+        self.elements[id_] = element
+        element.place(*args, **kwargs)
+
+    def pack(self, id_: str, element, *args, **kwargs) -> None:
+        """Размещает объект element.
+
+        Аргументы:
+            id_:        Уникальный идентификатор элемента.
+            element:    Элемент, который необходимо разместить.
+            *args:      Аргументы element.pack().
+            **kwargs:   Позиционные аргументы element.pack().
+        """
+        if id_ in self.elements:
+            raise ValueError
+
+        self.elements[id_] = element
+        element.pack(*args, **kwargs)
+
+    def __getattribute__(self, id_: str):
+        """Получает элемент по его ID.
+
+        Аргументы:
+            id_:    Уникальный идентификатор элемента.
+
+        Возвращаемое значение: Элемент.
+        """
+        if id_ in ["pack", "place", "tk_window", "elements"]:
+            return object.__getattribute__(self, id_)
+
+        return object.__getattribute__(self, "elements")[id_]
+
+
+class MessengerClient:
+    """Основной класс."""
+
+    MAIN_BACKGROUND = "#3a3a3a"
+    SELECT_FOREGROUND = "#555"
+    MAIN_FOREGROUND = "#dfdfdf"
+    SECOND_BACKGROUND = "#c9c9c9"
+    THIRD_BACKGROUND = "#bfbfbf"
+    _RECEIVE_SLEEP_TIME = 1 / 60
+
+    def __init__(self) -> None:
+        """Инициализация класса."""
+        self.root: Optional[tk.Tk] = None
+        self.win: Window = Window(self.root)
+        self._sock: socket = socket(AF_INET, SOCK_DGRAM)
+        self._sock.connect(("127.0.0.1", 7505))
+        self.__sended: list = []
+        self.__received: list = []
+        self._logins: dict = {}
+        self._userid_selected: int = -1
+
+        self.main()
+
+        Thread(target=self.receive, daemon=True).start()
+
+    def show_error(self, title: str, message: str) -> None:
+        """Показывает ошибку.
+
+        Аргументы:
+            title:      Заголовок ошибки.
+            message:    Описание ошибки.
+        """
+        title = f"ОШИБКА: {title}"
+        ltitle = len(title)
+        lmessage = len(message)
+        mlen = max(ltitle, lmessage)
+        dtitle = (mlen - ltitle) / 2
+        dmessage = (mlen - lmessage) / 2
+        print("-" * (mlen + 4))
+        print(f"|{' ' * (floor(dtitle) + 1)}{title}{' ' * (ceil(dtitle) + 1)}|")
+        print("-" * (mlen + 4))
+        print(f"|{' ' * (floor(dmessage) + 1)}{message}{' ' * (ceil(dmessage) + 1)}|")
+        print("-" * (mlen + 4))
+
+    def send(self, message: str) -> None:
+        """Отправляет сообщение message на сервер.
+
+        Аргументы:
+            message:    Сообщение.
+        """
+        self._sock.send(dumps(message).encode("utf8"))
+
+    def user_selected(self) -> None:
+        """Обработчик события выбора пользователя.
+
+        Аргументы:
+            wid:    Виджет Listbox Tkinter.
+        """
+        try:
+            listbox = self.win.userlist
+        except KeyError:
+            return
+
+        inv_logins = {val: key for key, val in self._logins.items()}
+        user_id = int(inv_logins[listbox.get(listbox.curselection()[0])[1:]])
+
+        if user_id != self._userid_selected:
+            self._userid_selected = user_id
+
+            for smsg in self.__sended:
+                if smsg[3] != self._userid_selected:
+                    continue
+
+                print("SENDED:", smsg[2])
+
+            for rmsg in self.__received:
+                if rmsg[1] != self._userid_selected:
+                    continue
+
+                print("RECEIVED:", rmsg[2])
+
+    def receive(self) -> None:
+        """Получает сообщения от сервера."""
+        print("IN")
+        while True:
+            print("ITER")
+            jdata = self._sock.recv(1024)
+            print("GET", jdata)
+            data = loads(jdata.decode("utf8"))
+            com = data[0]
+            print("Data:", data)
+
+            if com == "register_status":
+                status = data[1]
+
+                if status == 0:
+                    self.send(["get_account_data"])
+                elif status == 1:
+                    self.show_error(
+                        "Неверный логин",
+                        "Указанный логин слишком короткий"
+                    )
+                elif status == 2:
+                    self.show_error(
+                        "Неверный логин",
+                        "Указанный логин слишком длинный"
+                    )
+                elif status == 3:
+                    self.show_error(
+                        "Неверный пароль",
+                        "Указанный пароль слишком короткий"
+                    )
+                elif status == 4:
+                    self.show_error(
+                        "Неверный логин",
+                        "Аккаунт с указанным логином уже существует"
+                    )
+            elif com == "login_status":
+                status = data[1]
+
+                if status == 0:
+                    self.send(["get_account_data"])
+                elif status == 1:
+                    self.show_error(
+                        "Неверный логин",
+                        "Указанный логин слишком короткий"
+                    )
+                elif status == 2:
+                    self.show_error(
+                        "Неверный логин",
+                        "Указанный логин слишком длинный"
+                    )
+                elif status == 3:
+                    self.show_error(
+                        "Неверный пароль",
+                        "Указанный пароль слишком короткий"
+                    )
+                elif status == 4:
+                    self.show_error(
+                        "Неверный логин",
+                        "Аккаунта с указанным логином не существует"
+                    )
+                elif status == 5:
+                    self.show_error(
+                        "Неверный пароль",
+                        "Пароль от аккаунта не подходит"
+                    )
+            elif com == "account_data":
+                adata = data[1]
+                self.__sended = adata[0]
+                self.__received = adata[1]
+                self._logins = adata[2]
+
+                for element in self.win.elements.values():
+                    element.destroy()
+
+                listbox = tk.Listbox(
+                    bg=self.MAIN_BACKGROUND,
+                    bd=0,
+                    font="Arial 16 bold",
+                    fg=self.MAIN_FOREGROUND,
+                    selectbackground=self.SELECT_FOREGROUND,
+                    selectmode=tk.SINGLE,
+                    activestyle=tk.NONE,
+                    highlightthickness=0
+                )
+                listbox.bind("<<ListboxSelect>>", lambda _: self.user_selected())
+                self.win.place(
+                    "userlist",
+                    listbox,
+                    relw=0.3,
+                    relh=1,
+                    anchor=tk.NW,
+                    x=5,
+                    y=5,
+                    w=-10,
+                    h=-10
+                )
+                scrollbar = ttk.Scrollbar(command=listbox.yview)
+                self.win.place("userlist_scrollbar", scrollbar, relx=0.3, anchor=tk.NW, relh=1)
+                listbox.config(yscrollcommand=scrollbar.set)
+
+                cnv = tk.Canvas(
+                    background=self.MAIN_BACKGROUND,
+                    bd=0,
+                    highlightthickness=0
+                )
+                self.win.place(
+                    "messages",
+                    cnv,
+                    relx=0.3,
+                    x=20,
+                    relw=0.7,
+                    relh=1,
+                    anchor=tk.NW,
+                    y=5,
+                    w=-25,
+                    h=-10
+                )
+
+                for user in self._logins.values():
+                    listbox.insert(tk.END, f" {user}")
+
+            sleep(self._RECEIVE_SLEEP_TIME)
+
+
+    def main(self):
+        """Основная функция клиента."""
+        self.root = tk.Tk()
+        self.root.wm_title("Messenger")
+        self.root.wm_geometry("1000x600")
+        self.win = Window(self.root)
+
+        style = ttk.Style(self.root)
+        style.theme_use("clam")
+        self.root.configure(bg=self.MAIN_BACKGROUND)
+        style.configure(
+            "TLabel",
+            background=self.MAIN_BACKGROUND,
+            foreground=self.MAIN_FOREGROUND
+        )
+        style.configure(
+            "TEntry",
+            background=self.MAIN_BACKGROUND
+        )
+        style.configure(
+            "TButton",
+            background=self.SECOND_BACKGROUND,
+            activebackground=self.THIRD_BACKGROUND
+        )
+
+        self.win.place(
+            "loadscreen_registration",
+            ttk.Label(text="Регистрация", font="Arial 24 bold"),
+            relx=0.5,
+            rely=0,
+            anchor=tk.N,
+            y=12
+        )
+        self.win.place(
+            "loadscreen_registration_login",
+            ttk.Label(text="Логин:", font="Arial 16 bold"),
+            relx=0,
+            rely=0.45,
+            x=16,
+            anchor=tk.W
+        )
+        self.win.place(
+            "loadscreen_registration_password",
+            ttk.Label(text="Пароль:", font="Arial 16 bold"),
+            relx=0,
+            rely=0.55,
+            x=16,
+            anchor=tk.W
+        )
+        self.win.place(
+            "loadscreen_registration_login_field",
+            ttk.Entry(font="Arial 12"),
+            relx=1,
+            rely=0.45,
+            x=-16,
+            anchor=tk.E
+        )
+        self.win.place(
+            "loadscreen_registration_password_field",
+            ttk.Entry(font="Arial 12"),
+            relx=1,
+            rely=0.55,
+            x=-16,
+            anchor=tk.E
+        )
+        self.win.place(
+            "loadscreen_registration_button",
+            ttk.Button(
+                text="Зарегестрироваться",
+                command=lambda: self.send([
+                    "register",
+                    self.win.loadscreen_registration_login_field.get(),
+                    self.win.loadscreen_registration_password_field.get()
+                ])
+            ),
+            relx=0,
+            rely=1,
+            anchor=tk.SW,
+            y=-12,
+            x=12
+        )
+        self.win.place(
+            "loadscreen_login_button",
+            ttk.Button(
+                text="Войти",
+                command=lambda: self.send([
+                    "login",
+                    self.win.loadscreen_registration_login_field.get(),
+                    self.win.loadscreen_registration_password_field.get()
+                ])
+            ),
+            relx=1,
+            rely=1,
+            anchor=tk.SE,
+            y=-12,
+            x=-12
+        )
+
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    MessengerClient()
---
+++ server.py
@@ -0,0 +1,330 @@
+"""Модуль сервера."""
+from base64 import b64encode
+from hashlib import sha256
+from json import dumps
+from json import loads
+from os import path
+from socket import AF_INET
+from socket import SOCK_DGRAM
+from socket import socket
+from sqlite3 import connect
+from typing import Optional
+from typing import Union
+
+from bcrypt import kdf
+
+PASSWORD_EXTRA_SALT = "Pu~w9cC+RV)Bfjnd1oSbLQhjwGP)mJ$R^%+DHp(u)LP@AgMq)dl&0T\
+(V$Thope)Q"
+
+
+def absolute(path_):
+    """Возвращает абсолютный путь из относительного."""
+    return path.join(path.dirname(path.realpath(__file__)), path_)
+
+
+def encrypt_password(user_id: int, password: str) -> str:
+    """Шифрует пароль.
+
+    Аргументы:
+        user_id:    ID пользователя.
+        password:   Пароль пользователя.
+
+    Возвращаемое значение:  Зашифрованный пароль.
+    """
+    user_id = str((user_id + 1) ** 3)
+    hashed_password = b64encode(kdf(
+        password=sha256(
+            password.encode("utf8")
+        ).digest() + PASSWORD_EXTRA_SALT.encode("utf8"),
+        salt=user_id.encode("utf8"),
+        desired_key_bytes=24,
+        rounds=16
+    )).decode("utf8").replace("=", "_")
+    return hashed_password
+
+
+class Database:
+    """Класс базы данных."""
+
+    def __init__(self, filepath: str) -> None:
+        """Инициализация базы данных.
+
+        Аргументы:
+            filepath:   Путь к базе данных.
+        """
+        self.con = connect(filepath)
+        self.cur = self.con.cursor()
+
+    def sql(
+        self,
+        sql_text: str,
+        format_: list = None,
+        noresult: bool = False
+    ) -> Union[bool, list, tuple]:
+        """Выполняет SQL код.
+
+        Аргументы:
+            sql_text:   SQL код.
+            format_:    Заменители '?' в SQL коде.
+            noresult:   Если включено, то результатом будет булевое значение.
+
+        Возвращаемое значение:
+            bool:   Если включено noresult, возвращает True, если результат
+                        выполнения SQL кода пустой, иначе False.
+            list:   Массив с результатами.
+            tuple:  Единственный результат.
+        """
+        code = ""
+        results = []
+
+        if format_ is None:
+            format_ = []
+        else:
+            format_ = format_[::-1]
+
+        for line in sql_text.split("\n"):
+            stripped_line = line.strip()
+
+            if len(stripped_line) == 0:
+                continue
+
+            code += stripped_line
+
+            if stripped_line.endswith(";"):
+                formats = [
+                    format_.pop()
+                    for _ in range(code.count("?"))
+                ]
+
+                cursor = self.cur.execute(code, tuple(formats))
+                results.append(cursor.fetchall())
+                code = ""
+
+        self.con.commit()
+
+        if noresult:
+            return all(len(result) == 0 for result in results)
+
+        if len(results) == 1:
+            return results[0]
+
+        return results
+
+    def reset_database(self) -> bool:
+        """Сбрасывает базу данных к начальному состоянию.
+
+        Возвращаемое значение: True, если удалось сбросить, иначе False.
+        """
+        return self.sql("""
+            DROP TABLE IF EXISTS users;
+            DROP TABLE IF EXISTS direct_messages;
+
+            CREATE TABLE users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL DEFAULT 0,
+                name VARCHAR(16) NOT NULL,
+                password VARCHAR(32) NOT NULL
+            );
+
+            CREATE TABLE direct_messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL DEFAULT 0,
+                sender INTEGER(32) NOT NULL,
+                content INTEGER(2048) NOT NULL,
+                receiver INTEGER(32) NOT NULL,
+                read BOOLEAN(1) NOT NULL DEFAULT 0
+            );
+        """, noresult=True)
+
+    def create_account(self, name: str, password: str) -> int:
+        """Создаёт аккаунт.
+
+        Аргументы:
+            name:       Логин аккаунта.
+            password:   Пароль от аккаунта.
+
+        Возвращаемое значение:  Статус выполнения.
+        """
+        if len(name) < 1:
+            return 1  # ERR_SHORT_NAME
+        if len(name) > 16:
+            return 2  # ERR_LONG_NAME
+
+        if len(password) < 6:
+            return 3  # ERR_SHORT_PASSWORD
+
+        if not self.sql("SELECT * FROM users WHERE name = ?;", [name], True):
+            return 4  # ERR_ACCOUNT_EXISTS
+
+        count = self.sql("SELECT COUNT(*) FROM users;")[0][0] + 1
+        hashed_password = encrypt_password(count, password)
+
+        self.sql(
+            "INSERT INTO users (name, password) VALUES (?, ?);",
+            [name, hashed_password]
+        )
+
+        return 0  # SUCCESSFULL
+
+    def login_account(self, name: str, password: str) -> int:
+        """Входит в аккаунт.
+
+        Аргументы:
+            name:       Логин аккаунта.
+            password:   Пароль от аккаунта.
+
+        Возвращаемое значение:  Статус выполнения.
+        """
+        if len(name) < 1:
+            return 1  # ERR_SHORT_NAME
+        if len(name) > 16:
+            return 2  # ERR_LONG_NAME
+
+        if len(password) < 6:
+            return 3  # ERR_SHORT_PASSWORD
+
+        result = self.sql("SELECT id FROM users WHERE name = ?;", [name])
+
+        if not result:
+            return 4  # ERR_ACCOUNT_NOT_EXISTS
+
+        result = result[0][0]
+
+        hashed_password = encrypt_password(result, password)
+
+        if self.sql(
+            "SELECT * FROM users WHERE name = ? AND password = ?;",
+            [name, hashed_password],
+            True
+        ):
+            return 5  # ERR_INCORRECT_PASSWORD
+
+        return 0
+
+    def get_account_data(self, name: str) -> tuple:
+        """Получает данные аккаунта.
+
+        Аргументы:
+            name:   Логин аккаунта.
+
+        Возвращаемое значение:  Отправленные и полученные сообщения.
+        """
+        account_id = self.sql("SELECT id FROM users WHERE name = ?;", [name])
+
+        if not account_id:
+            return ()
+
+        account_id = account_id[0][0]
+
+        sended = self.sql(
+            "SELECT * FROM direct_messages WHERE sender = ?;",
+            [account_id]
+        )
+        received = self.sql(
+            "SELECT * FROM direct_messages WHERE receiver = ?;",
+            [account_id]
+        )
+
+        usernames = []
+        usernames_logins = {}
+
+        for smsg in sended:
+            if smsg[3] not in usernames:
+                usernames.append(smsg[3])
+
+        for rmsg in received:
+            if rmsg[1] not in usernames:
+                usernames.append(rmsg[1])
+
+        print(usernames)
+        for uname in usernames:
+            usernames_logins[uname] = self.sql(
+                "SELECT name FROM users WHERE id = ?;",
+                [uname]
+            )[0][0]
+
+        return (sended, received, usernames_logins)
+
+    def close(self):
+        """Закрывает базу данных."""
+        self.con.close()
+
+
+class NetworkedClient:
+    """Класс клиента."""
+
+    def __init__(self, sock: socket, addr: tuple[str, int]) -> None:
+        self.sock: socket = sock
+        self.addr: tuple[str, int] = addr
+        self.login: Optional[str] = None
+        self.password: Optional[str] = None
+
+    def send(self, message: Union[list, dict]) -> None:
+        """Отправляет сообщение клиенту.
+
+        Аргументы:
+            message:    Сообщение.
+        """
+        print("Отправлено клиенту:", message)
+        self.sock.sendto(dumps(message).encode("utf8"), self.addr)
+
+    def receive(self, jdata: bytes) -> None:
+        """Получает сообщение от клиента.
+
+        Аргументы:
+            jdata:  Данные от клиента.
+        """
+        data = loads(jdata.decode("utf8"))
+
+        print("Получено от клиента:", data)
+
+        com = data[0]
+        args = data[1:]
+
+        if com == "register":
+            status = dtb.create_account(*args[:2])
+            self.send(["register_status", status])
+
+            if status == 0:
+                self.login, self.password = args[:2]
+        elif com == "login":
+            status = dtb.login_account(*args[:2])
+            self.send(["login_status", status])
+
+            if status == 0:
+                self.login, self.password = args[:2]
+        elif not (self.login is None and self.password is None):
+            if com == "get_account_data":
+                self.send(["account_data", dtb.get_account_data(self.login)])
+
+
+dtb = Database(absolute("messenger.db"))
+
+
+def main():
+    """Основная функция."""
+    clients = {}
+
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.bind(("0.0.0.0", 7505))
+
+    print("Сервер запущен")
+
+    with sock:
+        while True:
+            try:
+                adrdata = sock.recvfrom(1024)
+            except (ConnectionResetError, ConnectionAbortedError):
+                pass
+
+            data = adrdata[0]
+            addr = adrdata[1]
+
+            if addr not in clients:
+                clients[addr] = NetworkedClient(sock, addr)
+
+            clients[addr].receive(data)
+
+    dtb.close()
+
+
+if __name__ == "__main__":
+    main()
