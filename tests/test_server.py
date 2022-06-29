994e543a45a73b99fd5ab036a6add64369825032
werryxgames <103959882+werryxgames@users.noreply.github.com>
2022-06-29 19:28:59 +0300

    Исправлены тесты

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,20 +1,34 @@
 import io
+import pytest
 
 from contextlib import redirect_stdout
 
 import main
 
-file = io.StringIO()
 
+@pytest.mark.parametrize("title, message, expected_result", [
+    ("", "", 12),  # Пустые параметры
+    ("Тестовая ошибка", "", 27),  # Только заголовок
+    ("", "Описание ошибки", 19),  # Только описание
+    ("Заголовок ошибки", "Описание ошибки", 28),  # Заголовок длиннее описания
+    ("Заголовок", "Описание ошибки..", 21),  # Описания длиной заголовка (+ 'ОШИБКА: ')
+    ("Заголовок", "Описание ошибки длинее заголовка", 36),  # Описание длинне заголовка
+    ("Title" * 1000, ("Description." * 10000)[:12008], 12012)  # Очень длинный заголовок и описание
+])
+def test_show_error(title, message, expected_result):
+    file = io.StringIO()
 
-def test_show_error():
     with redirect_stdout(file):
-        main.MessengerClient.show_error("Тест", "Тестовая ошибка")
+        main.MessengerClient.show_error(title, message)
 
-    assert file.getvalue() == """\
--------------------
-|  ОШИБКА: Тест   |
--------------------
-| Тестовая ошибка |
--------------------
-"""
+    lines = file.getvalue().splitlines()
+
+    print("\n".join(lines))
+
+    assert lines[0] == lines[2] == lines[4]
+    assert "ОШИБКА: " in lines[1]
+    assert lines[1].startswith("| ") and lines[1].endswith(" |")
+    assert lines[3].startswith("| ") and lines[3].endswith(" |")
+    assert all(char == "-" for char in lines[0])
+    assert len(lines[0]) == len(lines[1]) == len(lines[3])
+    assert len(lines[0]) == expected_result
